139fde94123b4ac04d7883bfda08a14c5bf77a05
Yoelys Figueredo Padrón <yoelys1982@gmail.com>
2023-06-27 13:09:09 -0300

    ejemplo de clases control de empleados

---
+++ Control_Empleados/Control_Empleados/Empleado.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Control_Empleados
+{
+    public class Empleado
+    {
+        private readonly string nombre;
+        private readonly string apellido;
+        private readonly string id;
+        private readonly string locker;
+        private readonly string banco;
+        private readonly Random random = new Random();
+
+        public Empleado(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            id = GenerarID();
+            locker = GenerarLocker();
+            banco = AsignaBanco();
+        }
+
+        public string Nip { get; set; }
+
+
+        private string GenerarID()
+        {
+            int numero;
+            string id = String.Empty;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numero = random.Next(10);
+                id += numero.ToString();
+            }
+
+            return id;
+        }
+
+        private string GenerarLocker()
+        {
+            int numero;
+            string locker = String.Empty;
+
+            for (int i = 0; i < 2; i++)
+            {
+                numero = random.Next(10);
+                locker += numero.ToString();
+            }
+
+            return locker;
+        }
+
+        private string AsignaBanco()
+        {            
+            int asignarBanco;
+            string banco = String.Empty;
+
+            asignarBanco = random.Next(1, 3);
+
+            switch (asignarBanco)
+            {
+                case 1:
+                    banco = "Santander";
+                    break;
+                case 2:
+                    banco = "BBVA";
+                    break;
+            }
+
+            return banco;
+        }
+
+        public override string ToString()
+        {
+            string mensaje = $"\nEmpleado: {nombre} {apellido}\nNúmero de empleado: {id}\nLocker No: {locker}\nBanco asignado: {banco}";
+            return mensaje;
+        }
+    }
+}
---
+++ Control_Empleados/Control_Empleados/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Empleados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            string nombre, apellido, nip;
+
+            Console.WriteLine("Bienbenido a Monsters Inc.\n");
+            Console.WriteLine("Ingrese los campos que se solicitan:\n");
+
+            Console.Write("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Apellido: ");
+            apellido = Console.ReadLine();
+            Console.Write("Digite su NIP para asignarlo a su tarjeta bancaria: ");
+            nip = Console.ReadLine();
+
+            Empleado empleado1 = new Empleado(nombre, apellido)
+            {
+                Nip = nip
+            };
+
+            Console.WriteLine(empleado1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
